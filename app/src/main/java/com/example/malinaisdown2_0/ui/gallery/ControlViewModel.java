2ad5ca81fdd6ca1e4e8669fd9e921c6c41eeef17
megolol <55526180+megolol@users.noreply.github.com>
2020-05-08 15:51:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/malinaisdown2_0/ui/gallery/ControlViewModel.java
@@ -0,0 +1,19 @@
+package com.example.malinaisdown2_0.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ControlViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ControlViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
