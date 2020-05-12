36ef77d0702445c849ba383adf055d1ae3d8d87e
megolol <55526180+megolol@users.noreply.github.com>
2020-05-12 09:24:19 +0300

    Update methods

--- app/src/main/java/com/example/malinaisdown2_0/ui/gallery/ControlFragment.java
+++ app/src/main/java/com/example/malinaisdown2_0/ui/gallery/ControlFragment.java
@@ -15,12 +15,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.malinaisdown2_0.R;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Properties;
+import com.example.malinaisdown2_0.ui.Functions;
 
 public class ControlFragment extends Fragment{
 
@@ -28,9 +23,6 @@ public class ControlFragment extends Fragment{
     private View controlView;
     private Button btn_shutdown;
     private Button btn_reboot;
-    private Session session;
-    private JSch jsch;
-    private ChannelExec channelssh;
     private String command, question, login, ip, pass;
 
 
@@ -48,129 +40,108 @@ public class ControlFragment extends Fragment{
 
 
         btn_shutdown = controlView.findViewById(R.id.btn_shutdown);
-        btn_shutdown.setOnClickListener(myShutdownListener);
-
         btn_reboot = controlView.findViewById(R.id.btn_reboot);
-        btn_reboot.setOnClickListener(myRebootListener);
+
 
         mSettings = this.getActivity().getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         ip = mSettings.getString(stringip, "");
         login = mSettings.getString(stringlogin, "");
         pass = mSettings.getString(stringpass, "");
 
-        return controlView;
+        assignListeners();
 
+        return controlView;
     }
 
-    View.OnClickListener myShutdownListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            question = "ВЫРУБАЕМ НАХ?";
-            command = "sudo shutdown -h now";
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(question)
-                    .setPositiveButton("DA EBA KANESHNO", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+    public void assignListeners() { //Метод назначающий клик листнер кнопкам
 
-                            sshSentCmd(); //Запуск метода отправки команды по ssh
-
-                            Toast.makeText(
-                                    getActivity(), "Ну все, хана малине",
-                                    Toast.LENGTH_LONG
-                            ).show();
-
-                        }
-                    })
-                    .setNegativeButton("NENE ASTANAVIS", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(
-                                    getActivity(), "Ну как хош братан",
-                                    Toast.LENGTH_LONG
-                            ).show();
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog alert = builder.create();
-            alert.setTitle("SHO");
-            alert.show();
-
-        }
-    };
-
-    public View.OnClickListener myRebootListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            command = "sudo shutdown -r now";
-            question = "РЕБУТАЕМ НАХ?";
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(question)
-                    .setPositiveButton("DA EBA KANESHNO", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-
-                            sshSentCmd(); //Запуск метода отправки команды по ssh
-
-                            Toast.makeText(
-                                    getActivity(), "Ну все, хана малине",
-                                    Toast.LENGTH_LONG
-                            ).show();
-
-                        }
-                    })
-                    .setNegativeButton("NENE ASTANAVIS", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(
-                                    getActivity(), "Ну как хош братан",
-                                    Toast.LENGTH_LONG
-                            ).show();
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog alert = builder.create();
-            alert.setTitle("SHO");
-            alert.show();
-
-        }
-    };
-
-
-    public void sshSentCmd() {
         new Thread(new Runnable() {
             @Override
             public void run() {
 
-                try {
-                    jsch = new JSch();
-                    session = jsch.getSession(login, ip, 22);
-                    session.setPassword(pass);
+                View.OnClickListener myShutdownListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        question = "ВЫРУБАЕМ НАХ?";
+                        command = "sudo shutdown -h now";
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage(question)
+                                .setPositiveButton("DA EBA KANESHNO", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
 
-                    // Avoid asking for key confirmation
-                    Properties prop = new Properties();
-                    prop.put("StrictHostKeyChecking", "no");
-                    session.setConfig(prop);
+                                        Functions.sshSentCmd(login, ip, pass, command); //Запуск метода отправки команды по ssh
 
-                    session.connect();
+                                        Toast.makeText(
+                                                getActivity(), "Ну все, хана малине",
+                                                Toast.LENGTH_LONG
+                                        ).show();
 
-                    channelssh = (ChannelExec) session.openChannel("exec");
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    channelssh.setOutputStream(baos);
+                                    }
+                                })
+                                .setNegativeButton("NENE ASTANAVIS", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(
+                                                getActivity(), "Ну как хош братан",
+                                                Toast.LENGTH_LONG
+                                        ).show();
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.setTitle("SHO");
+                        alert.show();
 
-                    channelssh.setCommand(command);
-                    channelssh.connect();
-                    channelssh.disconnect();
+                    }
+                };
+                btn_shutdown.setOnClickListener(myShutdownListener);
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                View.OnClickListener myRebootListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        command = "sudo shutdown -r now";
+                        question = "РЕБУТАЕМ НАХ?";
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage(question)
+                                .setPositiveButton("DA EBA KANESHNO", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        Functions.sshSentCmd(login, ip, pass, command); //Запуск метода отправки команды по ssh
+
+                                        Toast.makeText(
+                                                getActivity(), "Ну все, хана малине",
+                                                Toast.LENGTH_LONG
+                                        ).show();
+
+                                    }
+                                })
+                                .setNegativeButton("NENE ASTANAVIS", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(
+                                                getActivity(), "Ну как хош братан",
+                                                Toast.LENGTH_LONG
+                                        ).show();
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.setTitle("SHO");
+                        alert.show();
+
+                    }
+                };
+
+                btn_reboot.setOnClickListener(myRebootListener);
+
+
             }
         }).start();
 
     }
-
-
 }
+
