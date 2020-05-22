371dc2575de52ae783daca8fb94c9ed9d7b28ba2
megolol <55526180+megolol@users.noreply.github.com>
2020-05-22 20:43:34 +0300

    design fixes

--- app/src/main/java/com/example/malinaisdown2_0/Functions.java
+++ app/src/main/java/com/example/malinaisdown2_0/Functions.java
@@ -1,6 +1,8 @@
 package com.example.malinaisdown2_0;
 
+import android.app.Activity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -51,6 +53,17 @@ public class Functions {
                 }
             }
         }).start();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
 
